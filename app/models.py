3c74cd30f9fa6d515dccef4d54fd8030aa4de99a
Tristan Ohlson <tsohlson@gmail.com>
2017-12-12 19:23:11 -0500

    Basic model done. General idea showing through

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 VANILLA_TOWN = 'VANILLA_TOWN'
 MERLIN = 'MERLIN'
@@ -19,15 +20,51 @@ ROLES = (
     (OBERON, 'Oberon'),
 )
 
-class Player(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=30)
-    role = models.CharField(
-        max_length=12,
-        choices=ROLES,
-    )
+
+class User(AbstractUser):
+    pass
+
+
+class GameManager(models.Manager):
+    def create_joinable_id(self):
+        pass
+
 
 class Game(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     joinable_id = models.CharField(max_length=4)
-    players = models.ManyToManyField(Player)
+    started = models.BooleanField(default=False)
+
+    def start(self):
+        self.started = True
+        self.save()
+
+    objects = GameManager()
+
+
+class PlayerManager(models.Manager):
+    def _create_player(self, user, game, role):
+        if not game:
+            raise ValueError('The game must be set')
+        if role not in ROLES:
+            raise ValueError(f'The role must be one of {ROLES}')
+
+        is_guest = user is None
+        player = self.model(user=user, game=game, role=role, is_guest=is_guest)
+        player.save(using=self._db)
+        return player
+
+    def create_player(self, user, game, role):
+        return self._create_player(user, game, role)
+
+    def create_guest_player(self, game, role):
+        return self._create_player(None, game, role)
+
+
+class Player(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    role = models.CharField(max_length=12, choices=ROLES)
+    is_guest = models.BooleanField()
+
+    objects = PlayerManager()
--- app/views.py
+++ app/views.py
@@ -3,16 +3,11 @@ import random
 from django.shortcuts import render, redirect
 
 
-games = set()
-
 def index(request):
     return render(request, 'index.html')
 
 
 def create_game(request):
-    # games.add(game_id)
-    # request.session['game'] = game_id
-    # request.session['name'] = request.POST.get('name')
     return render(request, 'create_game.html')
 
 
@@ -20,5 +15,5 @@ def join_game(request):
     return render(request, 'join_game.html')
 
 
-def lobby(request, game_id):
-    return render(request, 'lobby.html', { 'game': game_id })
+def lobby(request, game, name):
+    return render(request, 'lobby.html', {'game': game, 'name': name})
