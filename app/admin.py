d7d9aab8dcb8bd2801f372ddb2dd405b4f7b7d86
Tristan Ohlson <tsohlson@gmail.com>
2017-12-25 01:50:03 -0500

    Gotta have that admin stuff

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
+from app.models import Game, Player, User
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(User, UserAdmin)
